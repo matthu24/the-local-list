0d667db4258112325307ded6ff4047bb491edc74
Matthew Hu <matthu24@gmail.com>
2018-04-05 22:57:27 -0700

    Add footer

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,12 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 import Cities from './cities/cities_container';
 import USMap from './maps/US_map_container';
 import NavBar from './navbar/navbar';
+import Footer from './footer/footer';
 
 const App = () => (
   <div>
     <NavBar/>
     <USMap/>
     <Cities/>
+    <Footer/>
 
   </div>
 );
---
+++ frontend/components/footer/footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Footer extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <div>
+        <div className='footer'>
+          <a className='linkedin' href="https://www.linkedin.com/in/matthew-hu-4a85752a/"><i className="fab fa-linkedin-in"></i></a>
+          <a className='github' href="https://www.linkedin.com/in/matthew-hu-4a85752a/"><i className="fab fa-github"></i></a>
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Footer;
