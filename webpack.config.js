d99562f65858b03dd426f7eb8a520c6eae674cac
Matthew Hu <matthu24@gmail.com>
2018-04-13 11:54:44 -0700

    test

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,6 @@ var prodPlugins = [
   new webpack.DefinePlugin({
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
   }),
-  new webpack.EnvironmentPlugin(['NODE_ENV']),
   new webpack.optimize.UglifyJsPlugin({
     compress: {
       warnings: true
