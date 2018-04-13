1b4b8abb73dc0d71a472fcf4a0caabd858a521cd
Matthew Hu <matthu24@gmail.com>
2018-04-13 11:47:05 -0700

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -21,11 +21,10 @@ var prodPlugins = [
   })
 ];
 
-// plugins = plugins.concat(
-//   process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
-// );
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
 
-plugins = plugins.concat(prodPlugins)
 
 module.exports = {
   entry: './frontend/entry.jsx',
