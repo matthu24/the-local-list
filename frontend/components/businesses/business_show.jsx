4a50d0b2b8c8eb8ec74cd09b5b72117ecf768cad
Matthew Hu <matthu24@gmail.com>
2018-04-13 20:01:33 -0700

    Add stuff to business show

--- frontend/components/businesses/business_show.jsx
+++ frontend/components/businesses/business_show.jsx
@@ -8,6 +8,7 @@ class BusinessShow extends React.Component{
 
   componentDidMount(){
     this.props.fetchSingleBusiness(this.props.match.params.businessId)
+    this.props.fetchAllCities();
   }
 
 
@@ -20,20 +21,60 @@ class BusinessShow extends React.Component{
       return null;
     }
     let imageFile = this.props.business.imageFile2;
+
+    let hours = " " + this.props.business.hours;
+    let capitalLetters = ['M','T','W','F','S'];
+    //parse hours
+    //add exclamation for every new line or 22 characters
+    for(let i=2;i< hours.length;i++){
+      if(capitalLetters.includes(hours[i])){
+        let hoursArray = hours.split("");
+        hoursArray[i-1] = '!';
+        hours = hoursArray.join("");
+      }
+    }
+
+    let address = this.props.business.address;
+    //get cities array
+    let cities = Object.values(this.props.cities).map(city => city.name  )
+    address = address.split(' ').map(word => {
+      let city = word.slice(0,word.length-1)
+      if(cities.includes(city)){
+        console.log('in here')
+        return `!${word}`
+      }else{
+        return word
+      }
+    }).join(" ")
+
+    let reservation = this.props.business.reservation ? 'true' : 'false';
+
+
+
     return(
       <div className='business-show'>
         <img className='business-image' src={imageFile}/>
         <div className='business-name'>
           {this.props.business.name}
         </div>
-        <div>
-          address, hours, phone number, reservation
+        <div className='business-hours'>
+          {hours.split('!').map((line,id) => <div key={id}>{line}</div>)}
         </div>
+        <div className='business-address'>
+          {address.split('!').map((line,id) => <div key={id}>{line}</div>)}
+        </div>
+        <div className='business-number'>
+          {this.props.business.phoneNumber}
+        </div>
+        <div className='business-number'>
+          reservations: {reservation}
+        </div>
+
         <div>
           map and two photos
         </div>
         <div>
-          number of likes, review form + reviews 
+          number of likes, review form + reviews
         </div>
 
         <div className='back' onClick={this.goBack}>back</div>
--- frontend/components/businesses/business_show_container.jsx
+++ frontend/components/businesses/business_show_container.jsx
@@ -3,14 +3,17 @@ import {connect} from 'react-redux';
 import BusinessShow from './business_show';
 import {fetchSingleBusiness } from '../../actions/business';
 import {fetchSingleCity} from '../../actions/city';
+import { fetchAllCities } from '../../actions/city';
 
 
 const mapStateToProps = (state,ownProps) => ({
-  business: state.entities.businesses[ownProps.match.params.businessId]
+  business: state.entities.businesses[ownProps.match.params.businessId],
+  cities: state.entities.cities
 });
 
 const mapDispatchToProps = dispatch => ({
-  fetchSingleBusiness: (id) => dispatch(fetchSingleBusiness(id))
+  fetchSingleBusiness: (id) => dispatch(fetchSingleBusiness(id)),
+  fetchAllCities: () => dispatch(fetchAllCities())
 });
 
 
