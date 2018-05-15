4bcc17982527fd9401be81cec85d65b1acfe4e6f
Matthew Hu <matthu24@gmail.com>
2018-05-15 12:13:46 -0700

    Style

--- frontend/components/favorites/favorites.jsx
+++ frontend/components/favorites/favorites.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import {favoritesParser} from '../../utils/favoritesParser';
+import BusinessTopTenItem from '../businesses/business_topten_item';
+import {likeCounter} from '../../utils/likeCounter';
+
 
 class Favorites extends React.Component{
   constructor(props){
@@ -11,13 +14,20 @@ class Favorites extends React.Component{
 
   render(){
     if(!this.props.businesses) return null;
+    //favorites parser takes businesses and userId, returns array of businesses that are liked by the input user
     let businesses = favoritesParser(this.props.businesses,this.props.currentUser.id);
-    console.log(businesses)
+
+
+    //businesses is an array of business objects
+    //sort array of objects by number of likes, then only take the top 5 or whatever
+    //returns [likeCount,business]
+    let orderedLikes = likeCounter(businesses);
+
     return(
       <div className='favorites-container'>
-        
+        <h1>Personal Favorites</h1>
         {
-          businesses.map((business,id) => <div key={id}>{business.name}</div>)
+          orderedLikes.map((business,id) => <BusinessTopTenItem key={id} number={id} likeCount={business[0]} business={business[1]}/>)
         }
       </div>
     )
