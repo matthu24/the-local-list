1638196bc5cca224423341d3a707719b7cb2ab8f
Matthew Hu <matthu24@gmail.com>
2018-05-28 23:51:11 -0700

    Add seed

--- frontend/components/maps/business_map.jsx
+++ frontend/components/maps/business_map.jsx
@@ -6,6 +6,7 @@ class BusinessMap extends React.Component{
   }
   componentDidMount(){
     const map = document.getElementById('business-map-container');
+      //create new map object, pass in html container element and coordinates/other attributes
       this.map = new google.maps.Map(map,{
         center: {
           lat: this.props.lat,
@@ -16,6 +17,7 @@ class BusinessMap extends React.Component{
       });
   }
   render(){
+    //create new marker object and pass in map object
     let marker = new google.maps.Marker({
     position: {lat: this.props.lat, lng: this.props.lng},
     map: this.map,
